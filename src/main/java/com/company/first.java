70fcaf222a3b0b6a936c61fedf627aa6e138fed7
olegkovtun <olegkovtun@gmail.com>
2024-05-27 20:34:31 +0300

    .

--- src/main/java/com/company/first.java
+++ src/main/java/com/company/first.java
@@ -1,17 +1,20 @@
 package com.company;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class first {
+
+    public static void main(String[] args) {
+        System.out.print("Вкажіть число: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if ((number >= 50) && (number <= 100)){
+            System.out.println("Число " + number + " міститься в проміжку (50; 100)");
+        }else {
+            System.out.println("Число " + number + " не міститься в проміжку (50; 100)");
         }
+
+
     }
 }
